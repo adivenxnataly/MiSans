13a69589594bbc610b6c1bbf767bcc824079eb99
Adinata <adinata.ch@proton.me>
2025-02-05 04:04:59 +0700

    update: add MiSansVF for flexible font
    this new script for enable MiSansVF with overlay to default MiSans-Dynamic.

--- customize.sh
+++ customize.sh
@@ -5,6 +5,9 @@ MISANS=$(find $MODPATH/MiSans -type f -name MiSans.ttf)
 if [ -z $MISANS ]; then
     ui_print "   MiSans font not found!"
     ui_print "   Please, re-download this module!"
+    ui_print "   Aborting.."
+    rm -rf $MODPATH
+    exit 1
 else
     ui_print " - patching to default font..."
     cp $MODPATH/MiSans/MiSans.ttf $MODPATH/system/fonts/Roboto-Regular.ttf
@@ -28,11 +31,70 @@ MISANS=$(find $MODPATH/MiSans -type f -name MiSans-Static.ttf)
 if [ -z $MISANS ]; then
     ui_print "   MiSans-Static font not found!"
     ui_print "   Please, re-download this module!"
+    ui_print "   Aborting.."
+    rm -rf $MODPATH
+    exit 1
 else
     ui_print " - patching to default font..."
     cp $MODPATH/MiSans/MiSans-Static.ttf $MODPATH/system/fonts/RobotoStatic-Regular.ttf
     sleep 2
-    rm -r $MODPATH/MiSans
+    ui_print "   done!"
+    rm -rf $MODPATH/MiSans
+fi
+
+sleep 2
+REPLACE="
+/system/fonts/RobotoStatic-Regular.ttf
+"
+for i in $REPLACE; do
+    if [ -r "$i" ]; then
+        chmod 644 "${MODPATH}${i}"
+        chcon u:object_r:system_file:s0 "${MODPATH}${i}"
+        chown root:root "${MODPATH}${i}"
+    fi
+done
+}
+
+misansvf(){
+MISANSVF=$(find $MODPATH/MiSansVF -type f -name MiSansVF.ttf)
+if [ -z $MISANSVF ]; then
+    ui_print "   MiSansVF font not found!"
+    ui_print "   Please, re-download this module!"
+    ui_print "   Aborting.."
+    rm -rf $MODPATH
+    exit 1
+else
+    ui_print "   Add MiSansVF.."
+    cp $MODPATH/MiSansVF/MiSansVF.ttf $MODPATH/system/fonts/MiSansVF.ttf
+    sleep 1
+    ui_print "   done!"
+    ui_print ""
+fi
+
+sleep 2
+REPLACE="
+/system/fonts/MiSansVF.ttf
+"
+for i in $REPLACE; do
+    if [ -r "$i" ]; then
+        chmod 644 "${MODPATH}${i}"
+        chcon u:object_r:system_file:s0 "${MODPATH}${i}"
+        chown root:root "${MODPATH}${i}"
+    fi
+done
+
+MISANSVFOV=$(find $MODPATH/MiSansVF -type f -name MiSansVF_Overlay.ttf)
+if [ -z $MISANSVFOV ]; then
+    ui_print "   MiSansVF_Overlay not found!"
+    ui_print "   Please, re-download this module"
+    ui_print "   Aborting.."
+    rm -rf $MODPATH
+    exit 1
+else
+    ui_print "   Add MiSansVF_Overlay.."
+    cp $MODPATH/MiSansVF/MiSansVF_Overlay.ttf $MODPATH/system/fonts/MiSansVF_Overlay.ttf
+    sleep 1
+    rm -rf $MODPATH/MiSansVF
     ui_print "   done!"
     ui_print ""
 fi
@@ -41,7 +103,7 @@ sleep 2
 ui_print "  Patching to default directory.."
 sleep 3
 REPLACE="
-/system/fonts/RobotoStatic-Regular.ttf
+/system/fonts/MiSansVF_Overlay.ttf
 "
 for i in $REPLACE; do
     if [ -r "$i" ]; then
@@ -60,7 +122,21 @@ if [ -z $FONTD ]; then
     ui_print "   Failed to patching MiSans!"
 else
     ui_print "   System Font found!"
+    ui_print "   continuing to patching MiSans.."
     font_dynamic
+    if [ -z $(find $MODPATH/ -type d -name theme_webview) ]; then
+        ui_print ""
+        ui_print "   Theme_Webview is already exist!"
+        rm -rf $MODPATH/files
+    else
+        ui_print "   Create Theme_Webview directory.."
+        sleep 1
+        cp $MODPATH/system/fonts/Roboto-Regular.ttf $MODPATH/data/system/fonts/theme_webview/Roboto-Regular.ttf
+        sleep 1
+        ui_print "   Webview Font: $(find $MODPATH/data/system/fonts/theme_webview -type f -name Roboto-Regular.ttf)"
+        ui_print "   success!"
+        rm -rf $MODPATH/files
+    fi
 fi
 
 ui_print ""
@@ -72,4 +148,15 @@ if [ -z $FONTS ]; then
 else
     ui_print "   Static font found!"
     font_static
+fi
+
+ui_print ""
+ui_print " • Find MiSansVF fonts:"
+MISVF=$(find /system/fonts -type -f -name MiSansVF.ttf)
+if [ -z $MISVF ]; then
+    sleep 2
+    ui_print "   MiSansVF not found, adding.."
+    misansvf
+else
+    ui_print "   MiSansVF is already exist!"
 fi
